4aae0f87e0cb3cacdf8e8a954261d75f8719c8d0
Karl Semich <0xloem@gmail.com>
2020-07-23 05:21:56 -0400

    work in progress

--- setup.bash
+++ setup.bash
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
+sudo apt-get install -y \
+      g++ \
+      autoconf \
+      cmake \
+      git \
+      libbz2-dev \
+      libcurl4-openssl-dev \
+      libssl-dev \
+      libncurses-dev \
+      libboost-thread-dev \
+      libboost-iostreams-dev \
+      libboost-date-time-dev \
+      libboost-system-dev \
+      libboost-filesystem-dev \
+      libboost-program-options-dev \
+      libboost-chrono-dev \
+      libboost-test-dev \
+      libboost-context-dev \
+      libboost-regex-dev \
+      libboost-coroutine-dev \
+      libtool \
+      doxygen \
+      ca-certificates \
+      fish
+git submodule update --init --recursive
 mkdir -p bitshares-core-build
 pushd bitshares-core-build
-cmake ../bitshares-core
+cmake ../bitshares-core/
 #make
 popd
-DEXBot/cli.py configure
+DEXBot/cli.py --configfile config/config.yml configure
